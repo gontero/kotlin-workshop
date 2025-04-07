b36e3742e1512a811af46ff38a094d53a5eb058c
RobertRyba <robert.ryba@auto1.com>
2025-04-07 13:35:29 +0200

    Object Expressions

---
+++ src/ObjectExpressions.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // val provider = AbstractBasicInfoProvider()
+    val provider = object : PersonInfoProvider {
+        override val providerInfo: String
+            get() = "new info provider"
+
+        fun getSessionId() = "id"
+    }
+    provider.printInfo(Person("John","Doe"))
+    provider.getSessionId()
+}
